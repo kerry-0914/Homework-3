92cf879af8c1be1275f24216a3aaaa0b04d152cf
kerry-0914 <53992618+kerry-0914@users.noreply.github.com>
2024-05-24 22:20:08 +0800

    Add files via upload

--- Markowitz.py
+++ Markowitz.py
@@ -123,16 +123,16 @@ class RiskParityPortfolio:
         """
         TODO: Complete Task 2 Below
         """
-        for index in df.index:
-            for columns in df.columns:
-                for date in range(index - self.lookback,index):
-                    sum=sum+df.at[date,columns]
-                    sumsum=sumsum+(df.at[date,columns])*(df.at[date,columns])
-                vol = {}
-                vol[columns] = (sumsum/50-(sum/50)**2)**0.5
-                volsum = volsum + vol[columns]
-            for columns in columns:
-                self.portfolio_weights.at[index,columns] = vol[columns]/volsum
+        # for index in df.index:
+        #     for columns in df.columns:
+        #         for date in range(index - self.lookback,index):
+        #             sum=sum+df.at[date,columns]
+        #             sumsum=sumsum+(df.at[date,columns])*(df.at[date,columns])
+        #         vol = {}
+        #         vol[columns] = (sumsum/50-(sum/50)**2)**0.5
+        #         volsum = volsum + vol[columns]
+        #     for columns in columns:
+        #         self.portfolio_weights.at[index,columns] = vol[columns]/volsum
         """
         TODO: Complete Task 2 Above
         """
